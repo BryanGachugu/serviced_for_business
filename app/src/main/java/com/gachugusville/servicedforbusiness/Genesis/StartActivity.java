5fbceed422ececac0597a0060bf4c41487ce9369
bryan <briangachugu5@gmail.com>
2021-03-28 14:06:34 +0300

    2021-03-28 14:06

--- app/src/main/java/com/gachugusville/servicedforbusiness/Genesis/StartActivity.java
+++ app/src/main/java/com/gachugusville/servicedforbusiness/Genesis/StartActivity.java
@@ -12,10 +12,18 @@ import com.gachugusville.development.servicedforbusiness.R;
 import com.gachugusville.servicedforbusiness.Dashboard.DashboardActivity;
 import com.gachugusville.servicedforbusiness.Registration.LogInActivity;
 import com.gachugusville.servicedforbusiness.Registration.SignUp;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class StartActivity extends AppCompatActivity {
+    private LocationRequest locationRequest;
+    public  static final  int GPS_CHECK_SETTINGS = 1001;
 
     @Override
 
@@ -29,7 +37,27 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
         MaterialButton btn_signUp = findViewById(R.id.btn_signUp);
 
-        
+        //TODO this extra parameters should be in the maps activity
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(2000);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext())
+                .checkLocationSettings(builder.build());
+
+        result.addOnCompleteListener(command -> {
+            try {
+                LocationSettingsResponse response = command.getResult(ApiException.class);
+                
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+        })
 
         if (FirebaseAuth.getInstance().getCurrentUser() != null) {
             startActivity(new Intent(this, DashboardActivity.class));
