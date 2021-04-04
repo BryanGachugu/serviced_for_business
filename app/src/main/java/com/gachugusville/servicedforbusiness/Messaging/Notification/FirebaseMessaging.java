b3b1c312092f7836b3e8a50bb160ddbfc82d43f2
bryan <briangachugu5@gmail.com>
2021-04-04 22:15:27 +0300

    2021-04-04 22:15

--- app/src/main/java/com/gachugusville/servicedforbusiness/Messaging/Notification/FirebaseMessaging.java
+++ app/src/main/java/com/gachugusville/servicedforbusiness/Messaging/Notification/FirebaseMessaging.java
@@ -20,83 +20,15 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class FirebaseMessaging extends FirebaseMessagingService {
-    String Uid;
-
     @Override
-    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+    public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (currentUser != null) {
-            Uid = currentUser.getUid();
+
+        if (remoteMessage.getNotification() != null) {
+            String title = remoteMessage.getNotification().getTitle();
+            String body = remoteMessage.getNotification().getBody();
+
+            NotificationHelper.displayNotification(getApplicationContext(), title, body);
         }
-
-        String sent = remoteMessage.getData().get("sent");
-        String user = remoteMessage.getData().get("user");
-
-        if (currentUser != null && sent.equals(currentUser.getUid())) {
-            if (!currentUser.getUid().equals(user)) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    sendOAndAboveNotification(remoteMessage);
-                } else sendNormalNotification(remoteMessage);
-            }
-        }
-    }
-
-    private void sendNormalNotification(RemoteMessage remoteMessage) {
-        String user = remoteMessage.getData().get("user");
-        String icon = remoteMessage.getData().get("icon");
-        String title = remoteMessage.getData().get("title");
-        String body = remoteMessage.getData().get("body");
-
-        RemoteMessage.Notification notification = remoteMessage.getNotification();
-        int i = Integer.parseInt(user.replaceAll("[\\D]", ""));
-        Intent intent = new Intent(this, ConversationActivity.class);
-        Bundle bundle = new Bundle();
-        bundle.putString("user_id", user);
-        intent.putExtras(bundle);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, i, intent, PendingIntent.FLAG_ONE_SHOT);
-        Uri defSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setSmallIcon(Integer.parseInt(icon))
-                .setContentText(body)
-                .setContentTitle(title)
-                .setAutoCancel(true)
-                .setSound(defSoundUri)
-                .setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        int j = 0;
-        if (i > 0) {
-            j = i;
-        }
-        notificationManager.notify(j, builder.build());
-    }
-
-    private void sendOAndAboveNotification(RemoteMessage remoteMessage) {
-        String user = remoteMessage.getData().get("user");
-        String icon = remoteMessage.getData().get("icon");
-        String title = remoteMessage.getData().get("title");
-        String body = remoteMessage.getData().get("body");
-
-        RemoteMessage.Notification notification = remoteMessage.getNotification();
-        int i = Integer.parseInt(user.replaceAll("[\\D]", ""));
-        Intent intent = new Intent(this, ConversationActivity.class);
-        Bundle bundle = new Bundle();
-        bundle.putString("user_id", user);
-        intent.putExtras(bundle);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, i, intent, PendingIntent.FLAG_ONE_SHOT);
-        Uri defSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-
-        OreoAndAboveNotification notification1 = new OreoAndAboveNotification(this);
-        Notification.Builder builder = notification1.getNotifications(title, body, pendingIntent, defSoundUri, icon);
-
-        int j = 0;
-        if (i > 0) {
-            j = i;
-        }
-        notification1.getManager().notify(j, builder.build());
-
     }
 }
