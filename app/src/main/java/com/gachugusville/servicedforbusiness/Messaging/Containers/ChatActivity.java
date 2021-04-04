eff11a459164a3dc112a92e3d4d65ffd49a4d0f9
bryan <briangachugu5@gmail.com>
2021-04-04 17:18:22 +0300

    2021-04-04 17:18

---
+++ app/src/main/java/com/gachugusville/servicedforbusiness/Messaging/Containers/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.gachugusville.servicedforbusiness.Messaging.Containers;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gachugusville.development.servicedforbusiness.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
--- app/src/main/java/com/gachugusville/servicedforbusiness/Messaging/Notification/FirebaseMessaging.java
+++ app/src/main/java/com/gachugusville/servicedforbusiness/Messaging/Notification/FirebaseMessaging.java
@@ -1,6 +1,9 @@
 package com.gachugusville.servicedforbusiness.Messaging.Notification;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -54,9 +57,45 @@ public class FirebaseMessaging extends FirebaseMessagingService {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, i, intent, PendingIntent.FLAG_ONE_SHOT);
         Uri defSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(Integer.parseInt(icon))
+                .setContentText(body)
+                .setContentTitle(title)
+                .setAutoCancel(true)
+                .setSound(defSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        int j = 0;
+        if (i > 0){
+            j = i;
+        }
+        notificationManager.notify(j, builder.build());
     }
 
     private void sendOAndAboveNotification(RemoteMessage remoteMessage) {
+        String user = remoteMessage.getData().get("user");
+        String icon = remoteMessage.getData().get("icon");
+        String title = remoteMessage.getData().get("title");
+        String body = remoteMessage.getData().get("body");
+
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        int i = Integer.parseInt(user.replaceAll("[\\D]", ""));
+        Intent intent = new Intent(this, ChatActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("user_id", user);
+        intent.putExtras(bundle);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, i, intent, PendingIntent.FLAG_ONE_SHOT);
+        Uri defSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        OreoAndAboveNotification notification1 = new OreoAndAboveNotification(this);
+        Notification.Builder builder = notification1.getNotifications(title, body, pendingIntent, defSoundUri, icon);
+
+        int j = 0;
+        if (i > 0){
+            j = i;
+        }
+        notification1.getManager().notify(j, builder.build());
 
     }
 }
