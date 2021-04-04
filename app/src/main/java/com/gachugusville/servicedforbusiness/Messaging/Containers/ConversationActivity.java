1303fb70867a882b51246c6fa1198e2e5e8b91e4
bryan <briangachugu5@gmail.com>
2021-04-04 19:45:27 +0300

    2021-04-04 19:45

--- app/src/main/java/com/gachugusville/servicedforbusiness/Messaging/Containers/ConversationActivity.java
+++ app/src/main/java/com/gachugusville/servicedforbusiness/Messaging/Containers/ConversationActivity.java
@@ -1,15 +1,16 @@
 package com.gachugusville.servicedforbusiness.Messaging.Containers;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.gachugusville.development.servicedforbusiness.R;
+import com.gachugusville.servicedforbusiness.Utils.ChatModel;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -33,6 +34,9 @@ public class ConversationActivity extends AppCompatActivity {
         edt_input_msg = findViewById(R.id.edt_input_msg);
         btn_send_msg = findViewById(R.id.btn_send_msg);
         messages_recycler_view = findViewById(R.id.messages_recycler_view);
+
+        ChatModel chatModel = new ChatModel();
+        chatModel.
 
 
     }
