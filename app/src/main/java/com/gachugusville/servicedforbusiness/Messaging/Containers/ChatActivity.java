2cb3b70568cf16ef806638f927c313470a049480
bryan <briangachugu5@gmail.com>
2021-04-05 02:30:34 +0300

    2021-04-05 02:30

--- app/src/main/java/com/gachugusville/servicedforbusiness/Messaging/Containers/ChatActivity.java
+++ app/src/main/java/com/gachugusville/servicedforbusiness/Messaging/Containers/ChatActivity.java
@@ -1,30 +1,40 @@
 package com.gachugusville.servicedforbusiness.Messaging.Containers;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.gachugusville.development.servicedforbusiness.R;
+import com.gachugusville.servicedforbusiness.Utils.Messaging.ChatDialog;
+import com.squareup.picasso.Picasso;
+import com.stfalcon.chatkit.commons.ImageLoader;
+import com.stfalcon.chatkit.commons.models.IDialog;
+import com.stfalcon.chatkit.dialogs.DialogsList;
 import com.stfalcon.chatkit.dialogs.DialogsListAdapter;
+
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
 
-    
+    private DialogsList dialogsList;
+    List<ChatDialog> dialogs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        dialogsList = findViewById(R.id.dialogsList);
 
-        DialogsListAdapter dialogsListAdapter = new DialogsListAdapter<>(dialogs, new ImageLoader() {
+        DialogsListAdapter dialogsListAdapter = new DialogsListAdapter(dialogs, new ImageLoader() {
             @Override
-            public void loadImage(ImageView imageView, String url) {
-                //If you using another library - write here your way to load image
-                Picasso.with(DialogsListActivity.this).load(url).into(imageView);
-            }
-        });
+            public void loadImage(ImageView imageView, @Nullable String url, @Nullable Object payload) {
 
-        dialogsListView.setAdapter(dialogsListAdapter);
+            }
+        })
+        dialogsList.setAdapter(dialogsListAdapter);
 
 
     }
