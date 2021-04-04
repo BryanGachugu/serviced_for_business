e922375c7b79802faa1d1fd5e5362a1c47a93c4c
bryan <briangachugu5@gmail.com>
2021-04-04 19:01:30 +0300

    2021-04-04 19:01

--- app/src/main/java/com/gachugusville/servicedforbusiness/Messaging/Containers/ConversationActivity.java
+++ app/src/main/java/com/gachugusville/servicedforbusiness/Messaging/Containers/ConversationActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.gachugusville.development.servicedforbusiness.R;
+
 public class ConversationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_conversation);
+        setContentView(com.gachugusville.development.servicedforbusiness.R.layout.activity_conversation);
     }
 }
