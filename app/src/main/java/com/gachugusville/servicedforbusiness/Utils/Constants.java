2868d4c9f6fd76efd51e361991e6b1ba3e864524
bryan <briangachugu5@gmail.com>
2021-04-05 18:24:29 +0300

    2021-04-05 18:24

--- app/src/main/java/com/gachugusville/servicedforbusiness/Utils/Constants.java
+++ app/src/main/java/com/gachugusville/servicedforbusiness/Utils/Constants.java
@@ -2,6 +2,6 @@ package com.gachugusville.servicedforbusiness.Utils;
 
 public class Constants {
     public final static String STREAM_API_KEY = "4ffsn3uepkea";
-    private final static String STREAM_API_TOKEN = "dn6hk75azfb5cpawn8632xtty3mnh7ks7ta48f33vz69dnfagh5mzpgn6ha968vw";
+    public final static String STREAM_API_TOKEN = "dn6hk75azfb5cpawn8632xtty3mnh7ks7ta48f33vz69dnfagh5mzpgn6ha968vw";
 
 }
