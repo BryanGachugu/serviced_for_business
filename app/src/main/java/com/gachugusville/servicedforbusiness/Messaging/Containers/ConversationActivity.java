c81c5ae860d3bcb03ab7ca1511de00ddf346138a
bryan <briangachugu5@gmail.com>
2021-04-05 12:39:28 +0300

    2021-04-05 12:39

--- app/src/main/java/com/gachugusville/servicedforbusiness/Messaging/Containers/ConversationActivity.java
+++ app/src/main/java/com/gachugusville/servicedforbusiness/Messaging/Containers/ConversationActivity.java
@@ -3,14 +3,23 @@ package com.gachugusville.servicedforbusiness.Messaging.Containers;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.gachugusville.development.servicedforbusiness.R;
+import com.gachugusville.servicedforbusiness.Utils.Messaging.Message;
+import com.google.firebase.auth.FirebaseAuth;
+import com.squareup.picasso.Picasso;
+import com.stfalcon.chatkit.commons.ImageLoader;
+import com.stfalcon.chatkit.messages.MessagesList;
+import com.stfalcon.chatkit.messages.MessagesListAdapter;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -22,6 +31,7 @@ public class ConversationActivity extends AppCompatActivity {
     private ImageButton btn_send_msg;
     private RecyclerView messages_recycler_view;
     private String customerUid;
+    private MessagesList messagesList;
 
 
     @Override
@@ -35,12 +45,17 @@ public class ConversationActivity extends AppCompatActivity {
         txt_online_status = findViewById(R.id.txt_online_status);
         edt_input_msg = findViewById(R.id.edt_input_msg);
         btn_send_msg = findViewById(R.id.btn_send_msg);
-        messages_recycler_view = findViewById(R.id.messages_recycler_view);
+        messagesList = findViewById(R.id.messagesList);
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setStackFromEnd(true);
-        messages_recycler_view.setHasFixedSize(true);
-        messages_recycler_view.setLayoutManager(linearLayoutManager);
+        String myUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        MessagesListAdapter<Message> adapter = new MessagesListAdapter<>(myUid, (imageView, url, payload) -> Picasso.get().load(url).into(imageView));
+        messagesList.setAdapter(adapter);
+        adapter.setOnMessageLongClickListener(new MessagesListAdapter.OnMessageLongClickListener<Message>() {
+            @Override
+            public void onMessageLongClick(Message message) {
+                Toast.makeText(ConversationActivity.this, "message to delete", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
